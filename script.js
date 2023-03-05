ff3303a227955701b9002067f7f0cc718d873e25
Teddylee89 <126998191+Teddylee89@users.noreply.github.com>
2023-03-05 13:39:44 +0900

    최종

--- script.js
+++ script.js
@@ -14,26 +14,28 @@ const months = ["January", "February", "March", "April", "May", "June", "July", 
 const renderCalendar = () => {
     let firstDayofMonth = new Date(currYear,currMonth, 1).getDay(); 
     lastDateofMonth = new Date(currYear,currMonth +1, 0).getDate(); 
-    lastDayofMonth = new Date(currYear,curMonth,lastDateofMonth).getDay(); //
+    lastDayofMonth = new Date(currYear,currMonth,lastDateofMonth).getDay(); //
     lastDateofLastMonth = new Date(currYear,currMonth, 0).getDate(); //
+    nextDateofMonth = new Date(currYear,currMonth +2, 0).getDate(); 
+
 
     let liTag = "";
 
     for (let i = firstDayofMonth; i>0 ; i--) {
-        liTag += '<li class="inactive"> ${lastDateofLastMonth -i + 1} </li>' ;   
+        liTag += `<li class="inactive"> ${lastDateofLastMonth -i + 1} </li>`;
     } 
  
     for (let i = 1; i<=lastDateofMonth; i++) {
         let isToday = i === date.getDate() && currMonth === new Date().getMonth()
-                && currYear === new Date().getFullYear() ? "active" :"";
-        liTag += '<li class="${isToday}">${i} </1i>' ;   
+                && currYear === new Date().getFullYear() ? "active" : "";
+        liTag += `<li class="${isToday}">${i} </1i>`;   
     }
 
-    for (let i = lastDayofMonth; i<6; i++) {
-        liTag += '<li class = "inactive"> ${i - lastDateofLastMonth +1}</li>';
+    for (let i = 1; i<6; i++) {
+        liTag += `<li class = "inactive"> ${i}</li>`;
     }
 
-    currentDate.innerText =  '$months[currMonth] ${currYear}';
+    currentDate.innerText = `${months[currMonth]} ${currYear}`;
     daysTag.innerHTML = liTag;
 }
 renderCalendar();
@@ -42,7 +44,7 @@ renderCalendar();
 
 prevNextIcon.forEach(icon => {
     icon.addEventListener("click",()=>{
-        currMonth = icon.id ==="prev" ? currMonth-1 : currMonth + 1;
+        currMonth = icon.id === "prev" ? currMonth-1 : currMonth + 1;
 
         if(currMonth<0 || currMonth > 11) {
             date = new Date(currYear, currMonth);
